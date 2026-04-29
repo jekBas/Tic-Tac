1e759be35d4d245258e97cb33dc9366544e4f994
Baskakov Yevhen <Eugene.Baskov.-nd@disneystreaming.com>
2026-04-29 11:33:13 +0300

    init commit

---
+++ src/main/java/com/example/TicTac/TicTacApplication.java
@@ -0,0 +1,13 @@
+package com.example.TicTac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacApplication.class, args);
+	}
+
+}
